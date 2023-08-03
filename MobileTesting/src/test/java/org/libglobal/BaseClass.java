443c7f4d7b4fdd00bee28a9f8e19bd5cb5b9c549
Ramkumarsethu8 <140097118+Ramkumarsethu8@users.noreply.github.com>
2023-08-03 23:08:46 +0530

    Add files via upload
    Mobile Testing

---
+++ MobileTesting/src/test/java/org/libglobal/BaseClass.java
@@ -0,0 +1,50 @@
+package org.libglobal;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.ElementOption;
+
+public class BaseClass {
+	public static AppiumDriver<MobileElement> driver;
+
+	public void launchApp() throws MalformedURLException {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("deviceName", "Galaxy A22");
+		capabilities.setCapability("platformName", "Android");
+		capabilities.setCapability("platformVersion", "12");
+		capabilities.setCapability("appPackage", "com.myglobalfair.buyerapp");
+		capabilities.setCapability("appActivity", "com.myglobalfair.buyerapp.MainActivity");
+		URL service = new URL("http://localhost:4723/wd/hub");
+		driver = new AppiumDriver<MobileElement>(service, capabilities);
+	}
+
+	public void implicitWait(long sec) {
+		driver.manage().timeouts().implicitlyWait(sec, TimeUnit.SECONDS);
+	}
+
+	public void elementSendKeys(WebElement element, String value) {
+		element.sendKeys(value);
+	}
+
+	public void elementClick(WebElement element) {
+		element.click();
+	}
+
+	public void elementTap(WebElement element) {
+		TouchAction action = new TouchAction(driver);
+		action.tap(ElementOption.element(element)).perform();
+	}
+
+	public void quitApp() {
+		driver.quit();
+	}
+
+}
